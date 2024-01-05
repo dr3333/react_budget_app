e98a5d26bdf1c4cceeddc445cdb685ff32fe3915
dr3333 <nnmdkh@gmail.com>
2024-01-05 09:58:00 -0600

    dev branch commit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,24 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+        const newBudgetValue = parseFloat(event.target.value);
+
+        if (!isNaN(newBudgetValue) && newBudgetValue >= 0) {
+            // Dispatch a 'SET_BUDGET' action with the new budget value
+            dispatch({ type: 'SET_BUDGET', payload: newBudgetValue });
+            setNewBudget(newBudgetValue); // Update the local state as well
+        } else {
+            alert('Please enter a valid positive number for the budget.');
+        }
+    };
+
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
